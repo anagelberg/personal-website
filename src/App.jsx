0d2884b93f177772804536730a0c3d93f95d333b
anagelberg <amy.nagelberg@gmail.com>
2023-09-18 21:34:23 -0700

    initial layout :tada:

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__overlay">
+        <h1>This is the start of something wonderful.</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
