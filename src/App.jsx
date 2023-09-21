17c5ecd70a9e294b1ea3a33637bf29c82d36dbbb
anagelberg <amy.nagelberg@gmail.com>
2023-09-20 21:36:45 -0700

    responsive to mobile

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <div className="app">
       <div className="app__overlay">
         <h1>This is the start of something wonderful.</h1>
+        <a href="https://github.com/anagelberg">View My Github</a>
       </div>
     </div>
   );
