984a7e17f0c9878b0675e0abbce35d6ed1e0435e
Amy Nagelberg <amy.nagelberg@gmail.com>
2023-09-22 17:22:12 -0700

    add git and linkedin icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.scss';
+import { ReactComponent as LinkedInIcon } from './assets/icons/linkedin.svg';
+import { ReactComponent as GithubIcon } from './assets/icons/github.svg';
 
 function App() {
   return (
     <div className="app">
       <div className="app__overlay">
         <h1>This is the start of something wonderful.</h1>
-        <a href="https://github.com/anagelberg">View My Github</a>
+        <div className='contact-container'>
+          <a href="https://github.com/anagelberg">
+            <GithubIcon class='icon' />
+          </a>
+          <a href="https://www.linkedin.com/in/amy-nagelberg/">
+            <LinkedInIcon class='icon' />
+          </a>
+        </div>
       </div>
     </div>
   );
