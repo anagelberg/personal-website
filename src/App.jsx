916da027324b95eeb41648e9992be789e58c589a
anagelberg <amy.nagelberg@gmail.com>
2023-09-28 20:48:23 -0700

    change grammar

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
         {/* <a href="#test">Will this scroll?</a> //yes */}
         <div className="app__content">
           <h1>This is the start of something wonderful.</h1>
-          <p>Please stay tuned as I build out my personal website.</p>
+          <p>Please stay tuned as I build out my portfolio website.</p>
           {/* <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis ipsam dolores voluptate, quia in eveniet laudantium placeat quas nesciunt doloribus at voluptas dolor odio rem fugiat. Incidunt, similique! Nulla doloremque explicabo at facilis corporis odit ipsam, modi aliquam. Impedit perspiciatis consequatur minima numquam odit similique ex veritatis accusamus natus possimus.</p>
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis ipsam dolores voluptate, quia in eveniet laudantium placeat quas nesciunt doloribus at voluptas dolor odio rem fugiat. Incidunt, similique! Nulla doloremque explicabo at facilis corporis odit ipsam, modi aliquam. Impedit perspiciatis consequatur minima numquam odit similique ex veritatis accusamus natus possimus.</p>
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis ipsam dolores voluptate, quia in eveniet laudantium placeat quas nesciunt doloribus at voluptas dolor odio rem fugiat. Incidunt, similique! Nulla doloremque explicabo at facilis corporis odit ipsam, modi aliquam. Impedit perspiciatis consequatur minima numquam odit similique ex veritatis accusamus natus possimus.</p>
