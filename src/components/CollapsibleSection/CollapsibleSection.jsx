68b04050aeb65cff32e22d3251bf1271d9e70878
anagelberg <amy.nagelberg@gmail.com>
2023-10-15 14:45:09 -0700

    add the basic animations to the collapsible sections

--- src/components/CollapsibleSection/CollapsibleSection.jsx
+++ src/components/CollapsibleSection/CollapsibleSection.jsx
@@ -1,25 +1,46 @@
 import './CollapsibleSection.scss';
-import { useState } from 'react';
-
+import { useState, useEffect, useRef } from 'react';
+import { ReactComponent as Arrow } from '../../assets/icons/chevron.svg';
 
 function CollapsibleSection({ title, contents }) {
-    const [collapsed, setCollapsed] = useState(false)
+    const [open, setOpen] = useState(false);
+    const myComponentRef = useRef();
 
+    // Function to scroll to the component
+    function scrollToComponent() {
+        if (myComponentRef.current) {
+            const element = myComponentRef.current;
+            const elementTop = element.getBoundingClientRect().top + window.scrollY;
+            window.scrollTo({ top: elementTop, behavior: 'smooth' });
+        }
+    }
+
+    // Function to add a delay before scrolling
+    function scrollToComponentWithDelay() {
+        setTimeout(scrollToComponent, 1000); // Delay of 1 second (1000 milliseconds)
+    }
 
 
     return (
-        <section className='section'>
+        <section className='section' ref={myComponentRef} onClick={scrollToComponentWithDelay}>
             <h1
-                // onClick={() => setCollapsed(currentVal => !currentVal)}
+                onClick={() => {
+                    setOpen(currentVal => !currentVal)
+                }
+                }
                 className='section__title'>
                 {title}
+                <Arrow className='section__arrow' />
             </h1>
-            {!collapsed &&
-                <div className='section__contents'>
+            <div
+                className={open
+                    ? 'section__content-container section__content-container--open'
+                    : 'section__content-container'}>
+                <div className={open ? 'section__contents' : 'section__contents--hidden'}>
                     {contents()}
                 </div>
-            }
-        </section>
+            </div>
+        </section >
     )
 }
 
